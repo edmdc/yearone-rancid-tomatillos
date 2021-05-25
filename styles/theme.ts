932ef44c4138a7693016c5dd6a4f1df33d9551eb
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 15:20:11 -0500

    Delete home page styles, add base @emotion styles

---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+import colors from './colors'
+
+export default {
+  colors,
+  gap: {
+    xxxs: '0.25rem',
+    xxs: '0.5rem',
+    xs: '1rem',
+    s: '1.5rem',
+    m: '2rem',
+    lg: '4rem',
+    xlg: '7rem',
+    xxlg: '10rem',
+    xxxlg: '12rem',
+  },
+  maxWidth: '88%',
+}
