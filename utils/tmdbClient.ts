c5aa0886c4b16ad1ea3b08a36659ce2486e14de0
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 17:10:04 -0500

    Refactor tmdb axios instance to a class

--- utils/tmdbClient.ts
+++ utils/tmdbClient.ts
@@ -1,9 +1,33 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
-const tmdbClient = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
-  timeout: 1200,
-  headers: { Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}` },
-})
+export default class TmdbClient {
+  client: AxiosInstance
 
-export default tmdbClient
+  constructor() {
+    this.client = axios.create({
+      baseURL: 'https://api.themoviedb.org/3',
+      timeout: 1200,
+      headers: { Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}` },
+    })
+  }
+
+  async getLatestReleases() {
+    try {
+      const { data } = await this.client.get(
+        '/discover/movie?language=en-US&sort_by=release_date.desc',
+      )
+      return data
+    } catch (error) {
+      return error
+    }
+  }
+
+  async getConfig() {
+    try {
+      const { data } = await this.client.get('/configuration')
+      return data
+    } catch (error) {
+      return error
+    }
+  }
+}
