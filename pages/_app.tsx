86d658e5292084203466840b34485b9a179da4dd
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 12:16:38 -0500

    Refactor global styles to emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Global } from '@emotion/react'
+import { AppProps } from 'next/app'
+import globalStyles from '../styles/global'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
