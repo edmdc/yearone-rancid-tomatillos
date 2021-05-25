0587eb2ffa63917b95df3ff5d20587c0bbf68841
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 15:20:11 -0500

    Delete home page styles, add base @emotion styles

--- styles/global.ts
+++ styles/global.ts
@@ -1,12 +1,16 @@
 import { css } from '@emotion/react'
 
 export default css`
+  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+  :root {
+    font-size: 62.5%;
+  }
+
   html,
   body {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
-      Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Manrope', sans-serif;
   }
 
   a {
