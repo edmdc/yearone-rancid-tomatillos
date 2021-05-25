8451187521ca7595ead24aceb95de7a25f842708
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 15:19:29 -0500

    Add @emotion's theme object interface

---
+++ emotion.d.ts
@@ -0,0 +1,38 @@
+import '@emotion/react'
+
+interface ColorLevels {
+  50: string
+  100: string
+  200: string
+  300: string
+  400: string
+  500: string
+  600: string
+  700: string
+  800: string
+  900: string
+}
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: {
+      yellow: ColorLevels
+      blue: ColorLevels
+      gray: ColorLevels
+      red: ColorLevels
+      teal: ColorLevels
+    }
+    gap: {
+      xxxs: string
+      xxs: string
+      xs: string
+      s: string
+      m: string
+      lg: string
+      xlg: string
+      xxlg: string
+      xxxlg: string
+    }
+    maxWidth: string
+  }
+}
