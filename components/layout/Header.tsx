61f1ee5edb51143d126d785581b6ac5c5f593f4d
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-25 15:25:20 -0500

    Add header styled component test

---
+++ components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export default styled.header`
+  background-color: ${(props) => props.theme.colors.yellow['500']};
+  max-width: ${(props) => props.theme.maxWidth};
+  margin: 0 auto;
+  font-weight: 900;
+  font-size: 2.4rem;
+`
