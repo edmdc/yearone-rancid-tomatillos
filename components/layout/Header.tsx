3ae09641786b8b074920d777a6248801b2d5a01a
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-26 15:26:03 -0500

    Small style change to header and html background

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled'
 
 export default styled.header`
-  background-color: ${(props) => props.theme.colors.yellow['500']};
+  background-color: ${(props) => props.theme.colors.yellow['900']};
   max-width: ${(props) => props.theme.maxWidth};
+  color: ${(props) => props.theme.colors.yellow['50']};
+  padding: 1rem;
   margin: 0 auto;
   font-weight: 900;
   font-size: 2.4rem;
--- styles/global.ts
+++ styles/global.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react'
+import colors from './colors'
 
 export default css`
   @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
@@ -11,6 +12,7 @@ export default css`
     padding: 0;
     margin: 0;
     font-family: 'Manrope', sans-serif;
+    background-color: ${colors.gray['50']};
   }
 
   a {
