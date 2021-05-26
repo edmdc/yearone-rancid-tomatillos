1206039c2675a5d242e1723aa15d488867eaa0da
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-26 16:21:45 -0500

    Add single movie route handler

--- utils/tmdbClient.ts
+++ utils/tmdbClient.ts
@@ -1,4 +1,22 @@
 import axios, { AxiosInstance } from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export interface Movie {
+  poster_path?: string
+  adult: boolean
+  overview: string
+  release_date: string
+  genre_ids: number[]
+  id: number
+  original_title: string
+  original_language: string
+  title: string
+  backdrop_path?: string
+  popularity: number
+  vote_count: number
+  video: boolean
+  vote_average: number
+}
 
 export default class TmdbClient {
   client: AxiosInstance
@@ -19,6 +37,16 @@ export default class TmdbClient {
       return data
     } catch (error) {
       return error
+    }
+  }
+
+  async getSingleMovie(req: NextApiRequest, res: NextApiResponse) {
+    const { movieId } = req.query
+    try {
+      const result = await this.client.get(`/movie/${movieId}`)
+      res.status(200).json(result.data)
+    } catch (err) {
+      res.status(err.response.status).json(err)
     }
   }
 
