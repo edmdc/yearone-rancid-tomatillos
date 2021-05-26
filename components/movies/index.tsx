f523f1d2a6fc83555310344b132b926ab738dce5
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-26 15:23:30 -0500

    Add movie grid to display movie api results

---
+++ components/movies/index.tsx
@@ -0,0 +1,131 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import styled from '@emotion/styled'
+import { css, useTheme } from '@emotion/react'
+import { Movie } from '../../utils/tmdbClient'
+import ImgFallback from './ImgFallback'
+
+const MoviesContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  max-width: ${(props) => props.theme.maxWidth};
+  grid-gap: ${(props) => props.theme.gap.xs};
+  margin: 4rem auto;
+`
+
+const MovieCard = styled.article`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 28rem;
+  max-width: 19rem;
+  position: relative;
+  margin: 2rem 0.5rem;
+  border-radius: 25px;
+  background: #e0e0e0;
+  box-shadow: 12px 12px 20px #cccccc, -12px -12px 20px #f4f4f4;
+`
+
+const Caption = styled.div`
+  position: absolute;
+  background-color: rgba(247, 247, 247, 0.6);
+  bottom: 0;
+  z-index: 12;
+  backdrop-filter: blur(3px);
+  width: 100%;
+  height: 8rem;
+`
+
+const Title = styled.p`
+  font-size: 1.8rem;
+  line-height: 2.7rem;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.gray['900']};
+  text-align: center;
+  width: 95%;
+  margin: 0.5rem auto;
+  padding: 0 0.5rem;
+`
+
+const RightChevron = () => {
+  const theme = useTheme()
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      css={css`
+        height: 2.8rem;
+        width: 2.8rem;
+        position: absolute;
+        z-index: 20;
+        right: 1rem;
+        bottom: 0.75rem;
+      `}
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="10"
+        css={css`
+          fill: ${theme.colors.gray['500']};
+          opacity: 0.8;
+          backdrop-filter: blur(2px);
+        `}
+      />
+      <path
+        css={css`
+          fill: ${theme.colors.gray['300']};
+        `}
+        d="M10.3 8.7a1 1 0 0 1 1.4-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.4-1.4l3.29-3.3-3.3-3.3z"
+      />
+    </svg>
+  )
+}
+
+const MoviesGrid = ({ movies }: { movies: Movie[] }): JSX.Element => {
+  const router = useRouter()
+  const rootImgSrc = 'https://image.tmdb.org/t/p/w185'
+  const formatTitle = (title: string): string =>
+    title.length > 28 ? title.slice(0, 24).concat('...') : title
+
+  const handleReroute = (event: React.MouseEvent) => {
+    router.push(`/movies/${event.currentTarget.id}`)
+  }
+
+  return (
+    <MoviesContainer>
+      {movies.map((movie) =>
+        movie.poster_path ? (
+          <MovieCard key={movie.id} onClick={handleReroute} id={String(movie.id)}>
+            <Image
+              src={rootImgSrc + movie.poster_path}
+              alt={`Poster for ${movie.title}`}
+              layout="fill"
+              objectFit="scale-down"
+              objectPosition="top"
+              css={css`
+                position: absolute;
+                z-index: 2;
+              `}
+            />
+            <Caption>
+              <Title>{formatTitle(movie.title)}</Title>
+              <RightChevron />
+            </Caption>
+          </MovieCard>
+        ) : (
+          <MovieCard key={movie.id}>
+            <ImgFallback />
+            <Caption>
+              <Title>{movie.title}</Title>
+              <RightChevron />
+            </Caption>
+          </MovieCard>
+        ),
+      )}
+    </MoviesContainer>
+  )
+}
+
+export default MoviesGrid
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,45 @@
+import { GetStaticProps } from 'next'
 import Header from '../components/layout/Header'
+import MoviesGrid from '../components/movies'
+import { Movie } from '../utils/tmdbClient'
 
-export default function Home(): JSX.Element {
-  return <Header>Hello World!</Header>
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch('http://localhost:8080/api/movies')
+  const data = await res.json()
+  if (!data.results) {
+    return {
+      props: {
+        movies: [],
+        error: data.errorMessage,
+      },
+    }
+  }
+  return {
+    props: {
+      movies: data.results,
+      error: '',
+    },
+  }
+}
+
+export default function Home({
+  movies,
+  error = '',
+}: {
+  movies: Movie[]
+  error?: string
+}): JSX.Element {
+  if (error)
+    return (
+      <>
+        <Header>Rancid Tomatillos</Header>
+        <h2>{error}</h2>
+      </>
+    )
+  return (
+    <>
+      <Header>Rancid Tomatillos</Header>
+      <MoviesGrid movies={movies} />
+    </>
+  )
 }
