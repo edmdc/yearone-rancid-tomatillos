fc3f71be0bb74054d1803bccea8a1451776cdddf
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-27 17:55:39 -0500

    Add some header typograhy

---
+++ styles/typography.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+
+export const H2 = styled.h2`
+  font-size: 4.8rem;
+  line-height: 7.2rem;
+  font-weight: 700;
+  margin: 0;
+`
+export const H3 = styled.h3`
+  font-size: 3.2rem;
+  line-height: 4.8rem;
+  font-weight: 600;
+  margin: 0;
+`
+
+export const H4 = styled.h4`
+  font-size: 2.4rem;
+  line-height: 3.6rem;
+  font-weight: 500;
+  margin: 0;
+`
