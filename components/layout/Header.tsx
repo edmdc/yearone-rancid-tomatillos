20b0b5199bfd7b86d19b7bb9c9d2d915a635f7e1
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-05-30 18:08:43 -0500

    Change color scheme, and update color references

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 
 export default styled.header`
-  background-color: ${(props) => props.theme.colors.yellow['900']};
+  background-color: ${(props) => props.theme.colors.green['900']};
   max-width: ${(props) => props.theme.maxWidth};
-  color: ${(props) => props.theme.colors.yellow['50']};
+  color: ${(props) => props.theme.colors.green['50']};
   padding: 1rem;
   margin: 0 auto;
   font-weight: 900;
