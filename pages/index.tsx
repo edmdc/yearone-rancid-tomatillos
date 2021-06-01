9b97a3c34ddbf0b6cf0bbb774ee569f7acf52d84
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-01 11:15:37 -0500

    Connect searchBox w/ search api route & render results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,81 @@
+import { css } from '@emotion/react'
 import { GetStaticProps } from 'next'
+import { ChangeEventHandler, useEffect, useRef, useState } from 'react'
+import Grid from '../components/layout/Grid'
 import Header from '../components/layout/Header'
-import MoviesGrid from '../components/movies'
-import { Movie } from '../utils/tmdbClient'
+import Row from '../components/layout/Row'
+import SearchBox from '../components/movies/SearchBox'
+import MovieThumbnail from '../components/movies/Thumbnail'
+import { H4 } from '../styles/typography'
+import { Movie, TmdbResponse } from '../utils/tmdbClient'
+
+export default function Home({
+  movies,
+  error = '',
+}: {
+  movies: Movie[]
+  error?: string
+}): JSX.Element {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [tmdbResults, setResults] = useState<TmdbResponse>({})
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    event.preventDefault()
+    setSearchQuery(event.currentTarget.value)
+  }
+
+  const searchMovie = async () => {
+    try {
+      const res = await fetch(`http://localhost:8080/api/search/${searchQuery}`)
+      const data = await res.json()
+      console.log(data)
+      setResults(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  if (error)
+    return (
+      <>
+        <Header>Rancid Tomatillos</Header>
+        <h2>{error}</h2>
+      </>
+    )
+  return (
+    <>
+      <Header>Rancid Tomatillos</Header>
+      <SearchBox
+        handleInputChange={handleInputChange}
+        searchQuery={searchQuery}
+        searchMovie={searchMovie}
+      />
+      {tmdbResults?.results ? (
+        <Grid columns={6}>
+          {tmdbResults.results.map((movie) => (
+            <MovieThumbnail movie={movie} key={movie.id} />
+          ))}
+        </Grid>
+      ) : (
+        <div
+          css={css`
+            max-width: 88%;
+            margin: 4rem auto;
+            h4 {
+              margin: 2rem 0;
+            }
+          `}
+        >
+          <H4>Trending</H4>
+          <Row>
+            {movies.map((movie) => (
+              <MovieThumbnail movie={movie} key={movie.id} />
+            ))}
+          </Row>
+        </div>
+      )}
+    </>
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch('http://localhost:8080/api/movies')
@@ -20,26 +94,4 @@ export const getStaticProps: GetStaticProps = async () => {
       error: '',
     },
   }
-}
-
-export default function Home({
-  movies,
-  error = '',
-}: {
-  movies: Movie[]
-  error?: string
-}): JSX.Element {
-  if (error)
-    return (
-      <>
-        <Header>Rancid Tomatillos</Header>
-        <h2>{error}</h2>
-      </>
-    )
-  return (
-    <>
-      <Header>Rancid Tomatillos</Header>
-      <MoviesGrid movies={movies} />
-    </>
-  )
 }
