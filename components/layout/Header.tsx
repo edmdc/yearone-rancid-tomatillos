a7eeceed4a0233a05cabda73bc1aaf55eb4b3bbe
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-01 11:16:23 -0500

    Add tmdb response interface, change header color

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 
 export default styled.header`
-  background-color: ${(props) => props.theme.colors.green['900']};
+  background-color: ${(props) => props.theme.colors.red['600']};
   max-width: ${(props) => props.theme.maxWidth};
-  color: ${(props) => props.theme.colors.green['50']};
+  color: ${(props) => props.theme.colors.red['50']};
   padding: 1rem;
   margin: 0 auto;
   font-weight: 900;
--- utils/tmdbClient.ts
+++ utils/tmdbClient.ts
@@ -21,6 +21,13 @@ export interface Movie {
   tagline?: string
 }
 
+export interface TmdbResponse {
+  page?: number
+  results?: Movie[]
+  total_pages?: number
+  total_results?: number
+}
+
 export default class TmdbClient {
   client: AxiosInstance
 
