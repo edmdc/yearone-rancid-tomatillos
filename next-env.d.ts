ab88639fdad3229eb732ddbfd6ce14c2dc668034
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-01 22:55:24 -0500

    Setup mongodb connection and declare namespace

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module NodeJS {
+  interface Global {
+    mongo: {
+      conn?: MongoClient
+      promise?: Promise<MongoClient>
+    }
+  }
+}
