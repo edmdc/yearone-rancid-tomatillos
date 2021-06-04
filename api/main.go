ea0175b6af5e4d6d366ac5d9f9123ac729f17027
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-04 15:54:09 -0500

    Add endpoint to get movie ratings

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,21 +15,46 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type MovieRating struct {
+  TmdbId int `json:"tmdbId,required"`
+  UpVotes uint `json:"upVotes"`
+  DownVotes uint `json:"downVotes"`
+}
 
 var RatingsCollection *mongo.Collection
 
 func main() {
 	utils.LoadEnv()
 	r := rootRouter()
-  connectDb()
+
+  ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+  defer cancel()
+
+  client := connectDb(ctx)
+  defer func() {
+    fmt.Println("Aha, just disconnected client")
+    if err := client.Disconnect(ctx); err != nil {
+      panic(err)
+    }
+  }()
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Howdy!"))
 	})
 
+  r.Route("/ratings", func(r chi.Router) {
+    r.Route("/{tmdbId}", func(r chi.Router) {
+      r.Use(RatingsCtx)
+      r.Get("/", getMovieRating)
+      r.Get("/up-vote", upVoteMovie)
+    })
+  })
 
 	fmt.Println("Starting server at http://localhost:8081")
 	log.Fatalln(http.ListenAndServe("localhost:8081", r))
@@ -56,19 +82,12 @@ func rootRouter() *chi.Mux {
 	return r
 }
 
-func connectDb() {
+func connectDb(ctx context.Context) *mongo.Client {
 		uri := os.Getenv("MONGODB_URI")
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
 		client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 		if err != nil {
 			panic(err)
 		}
-		defer func() {
-			if err = client.Disconnect(ctx); err != nil {
-				panic(err)
-			}
-		}()
 		// Ping the primary
 		if err := client.Ping(ctx, readpref.Primary()); err != nil {
 			panic(err)
@@ -76,4 +95,37 @@ func connectDb() {
 		fmt.Println("Successfully connected and pinged.")
 
     RatingsCollection = client.Database("yearone-rancid-tomatillos").Collection("Ratings")
+
+    return client
 }
+
+func RatingsCtx(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    tmdbId := chi.URLParam(r, "tmdbId")
+    var movieRating bson.M
+    err := RatingsCollection.FindOne(r.Context(), bson.D{{"tmdbId", tmdbId}}).Decode(&movieRating)
+    if err != nil {
+      utils.ErrorHandler(w, err.Error(), http.StatusNotFound)
+      return
+    }
+    ctx := context.WithValue(r.Context(), "movie", movieRating)
+    next.ServeHTTP(w, r.WithContext(ctx))
+  })
+}
+
+func getMovieRating(w http.ResponseWriter, r *http.Request) {
+  ctx := r.Context()
+  movieRating, ok := ctx.Value("movie").(*MovieRating)
+  if !ok {
+    utils.ErrorHandler(w, "Could not process entity", http.StatusUnprocessableEntity)
+    return
+  }
+  res, err := json.Marshal(movieRating)
+  if err != nil {
+    utils.ErrorHandler(w, "Server Error", http.StatusInternalServerError)
+    return
+  }
+
+  w.Write(res)
+}
+
