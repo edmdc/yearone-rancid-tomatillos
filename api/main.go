cde99065f282ebee4e15be49693a8fb568e6964f
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-06 16:38:58 -0500

    Create new rating if none exists when upvoting, fmt file

--- api/main.go
+++ api/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"os"
 	// "strings"
@@ -16,15 +17,17 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type MovieRating struct {
-  TmdbId int `json:"tmdbId,required"`
-  UpVotes uint `json:"upVotes"`
-  DownVotes uint `json:"downVotes"`
+	TmdbId    int                `json:"tmdbid,required"`
+	UpVotes   uint               `json:"upVotes"`
+	DownVotes uint               `json:"downVotes"`
+	Id        primitive.ObjectID `json:"_id,omitempty"`
 }
 
 var RatingsCollection *mongo.Collection
@@ -33,28 +36,29 @@ func main() {
 	utils.LoadEnv()
 	r := rootRouter()
 
-  ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
 
-  client := connectDb(ctx)
-  defer func() {
-    fmt.Println("Aha, just disconnected client")
-    if err := client.Disconnect(ctx); err != nil {
-      panic(err)
-    }
-  }()
+	client := connectDb(ctx)
+	defer func() {
+		fmt.Println("Aha, just disconnected client")
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Howdy!"))
 	})
 
-  r.Route("/ratings", func(r chi.Router) {
-    r.Route("/{tmdbId}", func(r chi.Router) {
-      r.Use(RatingsCtx)
-      r.Get("/", getMovieRating)
-      r.Get("/up-vote", upVoteMovie)
-    })
-  })
+	r.Route("/ratings", func(r chi.Router) {
+		r.Route("/{tmdbId}", func(r chi.Router) {
+			r.Use(RatingsCtx)
+			r.Get("/", GetMovieRating)
+			// r.Post("/create", CreateMovieRating)
+			r.Get("/upvote", UpVoteMovie)
+		})
+	})
 
 	fmt.Println("Starting server at http://localhost:8081")
 	log.Fatalln(http.ListenAndServe("localhost:8081", r))
@@ -83,49 +87,98 @@ func rootRouter() *chi.Mux {
 }
 
 func connectDb(ctx context.Context) *mongo.Client {
-		uri := os.Getenv("MONGODB_URI")
-		client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-		if err != nil {
-			panic(err)
-		}
-		// Ping the primary
-		if err := client.Ping(ctx, readpref.Primary()); err != nil {
-			panic(err)
-		}
-		fmt.Println("Successfully connected and pinged.")
+	uri := os.Getenv("MONGODB_URI")
+	fmt.Println(uri)
+	uri = "mongodb://127.0.0.1:27017"
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err.Error())
+	}
+	// Ping the primary
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Successfully connected and pinged.")
 
-    RatingsCollection = client.Database("yearone-rancid-tomatillos").Collection("Ratings")
+	RatingsCollection = client.Database("yearone-rancid-tomatillos").Collection("Ratings")
 
-    return client
+	return client
 }
 
 func RatingsCtx(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    tmdbId := chi.URLParam(r, "tmdbId")
-    var movieRating bson.M
-    err := RatingsCollection.FindOne(r.Context(), bson.D{{"tmdbId", tmdbId}}).Decode(&movieRating)
-    if err != nil {
-      utils.ErrorHandler(w, err.Error(), http.StatusNotFound)
-      return
-    }
-    ctx := context.WithValue(r.Context(), "movie", movieRating)
-    next.ServeHTTP(w, r.WithContext(ctx))
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tmdbId, err := strconv.Atoi(chi.URLParam(r, "tmdbId"))
+		if err != nil {
+			utils.ErrorHandler(w, "Invalid URL parameter: tmdbId should be a number string", http.StatusBadRequest)
+			return
+		}
+		var movieRating MovieRating
+		err = RatingsCollection.FindOne(r.Context(), bson.D{{"tmdbid", tmdbId}}).Decode(&movieRating)
+		if err != nil {
+			ctx := context.WithValue(r.Context(), "tmdbid", tmdbId)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+		ctx := context.WithValue(r.Context(), "movie", movieRating)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
-func getMovieRating(w http.ResponseWriter, r *http.Request) {
-  ctx := r.Context()
-  movieRating, ok := ctx.Value("movie").(*MovieRating)
-  if !ok {
-    utils.ErrorHandler(w, "Could not process entity", http.StatusUnprocessableEntity)
-    return
-  }
-  res, err := json.Marshal(movieRating)
-  if err != nil {
-    utils.ErrorHandler(w, "Server Error", http.StatusInternalServerError)
-    return
-  }
+func GetMovieRating(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	movieRating := ctx.Value("movie")
+	if movieRating == nil {
+		utils.ErrorHandler(w, "No rating found for this movie", http.StatusNotFound)
+		return
+	}
 
-  w.Write(res)
+	res, err := json.Marshal(movieRating)
+	if err != nil {
+		utils.ErrorHandler(w, "Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
 }
 
+func UpVoteMovie(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	movieRating := ctx.Value("movie")
+	if movieRating == nil {
+	  CreateMovieRating(w, r, "up")
+	  return
+	}
+}
+
+func CreateMovieRating(w http.ResponseWriter, r *http.Request, vote string) {
+	ctx := r.Context()
+	tmdbId := ctx.Value("tmdbid").(int)
+	var rating MovieRating
+
+	if vote == "up" {
+		rating = MovieRating{TmdbId: tmdbId, UpVotes: 1, DownVotes: 0}
+	}
+
+	if vote == "down" {
+		rating = MovieRating{TmdbId: tmdbId, UpVotes: 0, DownVotes: 1}
+	}
+
+	dbResult, err := RatingsCollection.InsertOne(r.Context(), rating)
+
+	if err != nil {
+		utils.ErrorHandler(w, err.Error(), http.StatusNotImplemented)
+		return
+	}
+
+	RatingsCollection.FindOne(r.Context(), bson.D{{"_id", dbResult.InsertedID}}).Decode(&rating)
+
+	if err != nil {
+		utils.ErrorHandler(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+  newRating, err := json.Marshal(rating)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(newRating)
+}
