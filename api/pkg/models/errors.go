3a73c6270b84d4238b8b0fc4877f43aaab00ac78
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-09 11:17:36 -0500

    Remove unused error func, format errors models file

--- api/pkg/models/errors.go
+++ api/pkg/models/errors.go
@@ -1,18 +1,12 @@
 package models
 
-import "net/http"
-
 type Error struct {
-  Message string `json:"message"`
-  Status  int     `json:"status"`
-  Name    string `json:"name"`
-  Error   error  `json:"-"`
-}
-
-func BindError() *Error {
-  return &Error{Status: http.StatusBadRequest, Message: "Error processing request. Could not bind value.", Name:"BIND_ERROR"}
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Name    string `json:"name"`
+	Error   error  `json:"-"`
 }
 
 func CustomError(name, message string, status int) *Error {
-  return &Error{Name: name, Message: message, Status: status}
+	return &Error{Name: name, Message: message, Status: status}
 }
