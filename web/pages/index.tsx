b1e8df6c4909ad0e927d2c3b9ea9406e47b1c243
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-09 15:56:22 -0500

    Add footer to pages & ability to clear search results

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,6 +8,27 @@ import Row from '@/components/layout/Row'
 import SearchBox from '@/components/movies/SearchBox'
 import MovieThumbnail from '@/components/movies/Thumbnail'
 import { H4 } from '@/styles/typography'
+import Footer from '@/components/layout/Footer'
+import styled from '@emotion/styled'
+
+const Results = styled.div`
+  max-width: ${(props) => props.theme.maxWidth};
+  margin: 6rem auto;
+  display: flex;
+  flex-flow: column;
+`
+
+const ClearButton = styled.button`
+  font-size: 1.8rem;
+  width: 18rem;
+  height: 5rem;
+  align-self: flex-end;
+  color: ${(props) => props.theme.colors.red['50']};
+  background-color: ${(props) => props.theme.colors.red['500']};
+  border: 0;
+  border-radius: 0.8rem;
+  padding: 1rem 2rem;
+`
 
 export default function Home({
   movies,
@@ -33,6 +54,10 @@ export default function Home({
     }
   }
 
+  const clearResults = () => {
+    setSearchQuery(() => '')
+    setResults(() => ({}))
+  }
   if (error)
     return (
       <>
@@ -49,11 +74,16 @@ export default function Home({
         searchMovie={searchMovie}
       />
       {tmdbResults?.results ? (
-        <Grid columns={6}>
-          {tmdbResults.results.map((movie) => (
-            <MovieThumbnail movie={movie} key={movie.id} />
-          ))}
-        </Grid>
+        <Results>
+          <ClearButton type="button" onClick={clearResults}>
+            Clear Search
+          </ClearButton>
+          <Grid columns={6}>
+            {tmdbResults.results.map((movie) => (
+              <MovieThumbnail movie={movie} key={movie.id} />
+            ))}
+          </Grid>
+        </Results>
       ) : (
         <div
           css={css`
@@ -72,6 +102,7 @@ export default function Home({
           </Row>
         </div>
       )}
+      <Footer />
     </>
   )
 }
