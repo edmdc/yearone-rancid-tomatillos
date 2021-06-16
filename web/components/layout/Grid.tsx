9b88f9c83648067c3c23b77f488742d08698e596
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-16 16:17:06 -0500

    Refactor Grid component for gap flexibility

--- web/components/layout/Grid.tsx
+++ web/components/layout/Grid.tsx
@@ -2,13 +2,16 @@ import styled from '@emotion/styled'
 
 interface GridProps {
   columns: number
+  rowGap: number
+  columnGap: number
 }
 
 const Grid = styled.section<GridProps>`
   display: grid;
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
   width: 100%;
-  row-gap: ${(props) => props.theme.gap.xlg};
+  row-gap: ${(props) => props.rowGap}rem;
+  column-gap: ${(props) => props.columnGap}rem;
   margin: 4rem auto;
   justify-items: center;
 `
