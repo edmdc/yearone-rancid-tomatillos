b78a73368965170214e78f6cd6f7f76b4d1a9f69
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-16 11:47:35 -0500

    Configure webpack dev middleware for docker use

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['image.tmdb.org'],
   },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
