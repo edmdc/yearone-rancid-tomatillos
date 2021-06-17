58db555e2ec137676d2cdfe217b81948eebd415d
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-16 22:14:51 -0500

    Add film icon as header app logo

--- web/components/icons/index.tsx
+++ web/components/icons/index.tsx
@@ -1,7 +1,15 @@
-import RightChevron from './RightChevron'
-import ThumbsDown from './ThumbsDown'
-import ThumbsUp from './ThumbsUp'
-import GithubIcon from './GithubIcon'
-import LinkedInIcon from './LinkedInIcon'
+import RightChevron from "./RightChevron"
+import ThumbsDown from "./ThumbsDown"
+import ThumbsUp from "./ThumbsUp"
+import GithubIcon from "./GithubIcon"
+import LinkedInIcon from "./LinkedInIcon"
+import FilmIcon from "./FilmIcon"
 
-export { RightChevron, ThumbsDown, ThumbsUp, GithubIcon, LinkedInIcon }
+export {
+  RightChevron,
+  ThumbsDown,
+  ThumbsUp,
+  GithubIcon,
+  LinkedInIcon,
+  FilmIcon,
+}
--- web/components/layout/Header.tsx
+++ web/components/layout/Header.tsx
@@ -1,25 +1,33 @@
-import Link from 'next/link'
-import styled from '@emotion/styled'
+import Link from "next/link"
+import styled from "@emotion/styled"
+import { FilmIcon } from "@/components/icons"
 
 const HeaderWrapper = styled.header`
-  background-color: ${(props) => props.theme.colors.red['600']};
+  background-color: ${(props) => props.theme.colors.red["600"]};
   max-width: ${(props) => props.theme.maxWidth};
-  padding: 1rem;
+  padding: 1.4rem;
   margin: 0 auto;
+`
 
-  button {
-    color: ${(props) => props.theme.colors.red['50']};
-    background-color: transparent;
+const Title = styled.a`
+  display: flex;
+  align-items: center;
+
+  span {
+    color: ${(props) => props.theme.colors.red["50"]};
     font-weight: 900;
     font-size: 2.4rem;
-    border: 0;
+    margin-left: 0.7rem;
   }
 `
 
 const Header = () => (
   <HeaderWrapper>
     <Link href="/">
-      <button type="button">Rancid Tomatillos</button>
+      <Title>
+        <FilmIcon />
+        <span>Rancid Tomatillos</span>
+      </Title>
     </Link>
   </HeaderWrapper>
 )
