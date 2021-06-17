e826b0e852007e36a292d973e00cc50b2d39b6a9
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-16 22:16:34 -0500

    Refactor icon styles for flexible sizes

--- web/styles/icons.ts
+++ web/styles/icons.ts
@@ -1,29 +1,40 @@
-import { css } from '@emotion/react'
-import theme from './theme'
+import { css } from "@emotion/react"
+import theme from "./theme"
 
 export const thumbIconStyles = css`
   margin: 0;
   width: 3.2rem;
   height: 3.2rem;
   border-radius: 5rem;
-  background-color: ${theme.colors.gray['600']};
+  background-color: ${theme.colors.gray["600"]};
   padding: 0.4rem;
 `
 
-export const socialIconStyles = css`
-  height: 3.3rem;
-  width: 3.3rem;
-  margin: 1rem;
-`
+export const iconSize = {
+  sm: css`
+    height: 2.7rem;
+    width: 2.7rem;
+  `,
+  md: css`
+    height: 3.3rem;
+    width: 3.3rem;
+  `,
+}
 
 export const iconColors = {
   primary: css`
-    fill: ${theme.colors.gray['50']};
+    fill: ${theme.colors.gray["50"]};
   `,
   secondary: css`
-    fill: ${theme.colors.gray['200']};
+    fill: ${theme.colors.gray["200"]};
   `,
   social: css`
-    fill: ${theme.colors.red['50']};
+    fill: ${theme.colors.red["50"]};
+  `,
+  red1: css`
+    fill: ${theme.colors.red["100"]};
+  `,
+  red2: css`
+    fill: ${theme.colors.red["300"]};
   `,
 }
