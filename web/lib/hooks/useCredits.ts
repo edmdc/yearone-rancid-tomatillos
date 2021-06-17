c858912f5ca45a0e681d41767cfbdb98954a3573
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-17 17:17:42 -0500

    Add custom hook to fetch movie credits

---
+++ web/lib/hooks/useCredits.ts
@@ -0,0 +1,28 @@
+import useSWR from "swr"
+import { Crew } from "../api/tmdbClient"
+import fetcher from "../utils/fetcher"
+
+const useCredits = (movieId: string) => {
+  const { data, error } = useSWR(
+    `http://localhost:8080/api/credits/${movieId}`,
+    fetcher,
+  )
+
+  const crewCopy = data?.crew?.reduce((acc: Crew[], member: Crew) => {
+    if (member.job === "Director") {
+      acc.unshift(member)
+      return acc
+    }
+    acc.push(member)
+    return acc
+  }, [])
+
+  return {
+    cast: data?.cast,
+    crew: crewCopy ?? [],
+    loading: !data || !error,
+    error,
+  }
+}
+
+export default useCredits
