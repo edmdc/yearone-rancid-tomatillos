35fa20a217f7b2cfe429d4ae8238a77f22fc0d02
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-06-22 17:22:42 -0500

    Add page result parameter to nextjs api/pages search route

---
+++ web/pages/search/[query]/[page].tsx
@@ -0,0 +1,90 @@
+import { GetStaticPaths, GetStaticProps } from "next"
+import Link from "next/link"
+import styled from "@emotion/styled"
+import TmdbClient, { Movie, TmdbResponse } from "@/lib/api/tmdbClient"
+import { H4 } from "@/styles/typography"
+import Grid from "@/components/layout/Grid"
+import SearchBox from "@/components/movies/SearchBox"
+import SearchResult from "@/components/movies/SearchResult"
+import PaginationBar from "@/components/movies/PaginationBar"
+import Layout from "@/components/layout"
+
+const Results = styled.div`
+  max-width: ${(props) => props.theme.maxWidth};
+  margin: 1rem auto;
+  display: flex;
+  flex-flow: column;
+`
+
+const HomeLink = styled.a`
+  font-size: 2.4rem;
+  color: ${(props) => props.theme.colors.red["400"]};
+  cursor: pointer;
+`
+
+export default function Search({
+  movieData,
+  error,
+}: {
+  movieData: TmdbResponse
+  error: string
+}) {
+  return (
+    <Layout>
+      <SearchBox short />
+      <Results>
+        {!movieData && !error && <span>Loading</span>}
+        {movieData?.results && (
+          <>
+            <PaginationBar
+              currentPage={movieData?.page}
+              totalPages={movieData?.total_pages}
+            />
+            <Grid columns={2} columnGap={1.8} rowGap={1.8}>
+              {movieData?.results.map((movie: Movie) => (
+                <SearchResult movie={movie} key={movie.id} />
+              ))}
+            </Grid>
+            )
+          </>
+        )}
+        {error && (
+          <>
+            <H4>{error}</H4>
+            <Link href="/">
+              <HomeLink>Take me Home ☞</HomeLink>
+            </Link>
+          </>
+        )}
+      </Results>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const tmdbClient = new TmdbClient()
+  const { query, page } = context.params
+  const data = await tmdbClient.searchMovies(
+    String(query),
+    Number.parseInt(String(page), 10),
+  )
+  if (!data) {
+    return {
+      props: {
+        movieData: {},
+        error: "Opps, something went wrong. Could not load results",
+      },
+    }
+  }
+  return {
+    props: {
+      movieData: data,
+      error: "",
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [],
+  fallback: true,
+})
