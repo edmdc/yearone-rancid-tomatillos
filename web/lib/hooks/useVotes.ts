a5f3ee6597b6f9fae749870fc123cfd653a86f17
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-07-01 10:05:58 -0500

    Use prod/dev env to determine api url

--- web/lib/hooks/useVotes.ts
+++ web/lib/hooks/useVotes.ts
@@ -1,28 +1,25 @@
 import useSWR, { mutate } from "swr"
+import fetcher from "../utils/fetcher"
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://rancid-tomatillos-api-v7ekia46ga-uc.a.run.app"
+    : "http://localhost:8081"
 
 const useVotes = (movieId: string) => {
   let errorCatch: string
-  const { data, error } = useSWR(
-    `${process.env.NEXT_PUBLIC_API_URL}/v1/ratings/${movieId}`,
-    fetcher,
-  )
-
-  console.log(process.env.NEXT_PUBLIC_API_URL)
+  const { data, error } = useSWR(`${apiUrl}/v1/ratings/${movieId}`, fetcher)
 
   const voteOnMovie = async (vote: "upvote" | "downvote") => {
     try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/v1/ratings/${movieId}/${vote}`,
-        {
-          method: "POST",
-        },
-      )
+      const res = await fetch(`${apiUrl}/v1/ratings/${movieId}/${vote}`, {
+        method: "POST",
+      })
       const updatedRating = await res.json()
       return updatedRating
     } catch (err) {
       errorCatch = err
+      return err
     }
   }
 
@@ -32,15 +29,9 @@ const useVotes = (movieId: string) => {
       downVotes: data?.downVotes ?? 0,
     },
     upVote: () =>
-      mutate(
-        `${process.env.NEXT_PUBLIC_API_URL}/v1/ratings/${movieId}`,
-        voteOnMovie("upvote"),
-      ),
+      mutate(`${apiUrl}/v1/ratings/${movieId}`, voteOnMovie("upvote")),
     downVote: () =>
-      mutate(
-        `${process.env.NEXT_PUBLIC_API_URL}/v1/ratings/${movieId}`,
-        voteOnMovie("downvote"),
-      ),
+      mutate(`${apiUrl}/v1/ratings/${movieId}`, voteOnMovie("downvote")),
     loading: !data && !error,
     error: error || errorCatch,
   }
